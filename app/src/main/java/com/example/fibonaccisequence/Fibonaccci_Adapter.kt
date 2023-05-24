8ffa358a3ff7df47b521a58983d8ea375e0d9523
MutindaVictoria <victoriaamutinda@gmail.com>
2023-05-24 19:56:00 +0300

    recycler views

---
+++ app/src/main/java/com/example/fibonaccisequence/Fibonaccci_Adapter.kt
@@ -0,0 +1,27 @@
+package com.example.fibonaccisequence
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fibonaccisequence.R
+
+class FibonacciAdapter(private val numbers: List<Int>) :
+    RecyclerView.Adapter<FibonacciAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fibonacciadapter, parent, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val number = numbers[position]
+        holder.numberTextView.text = number.toString()
+    }
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val numberTextView: TextView = itemView.findViewById(R.id.RecyclerView)
+    }
+}
---
+++ app/src/main/java/com/example/fibonaccisequence/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fibonaccisequence
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FibonacciAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.RecyclerView)
+        adapter = FibonacciAdapter(getFibonacciNumbers())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+    private fun getFibonacciNumbers(): List<Int> {
+        val fibonacciNumbers = mutableListOf(0, 1)
+        for (i in 2 until 100) {
+            val nextNumber = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2]
+            fibonacciNumbers.add(nextNumber)
+        }
+        return fibonacciNumbers
+    }
+}
